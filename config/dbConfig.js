4ceb83f235cd8cd43c0a35c6a07844f72cb9ba0f
pradeepb014@gmail.com <pradeepb014@gmail.com>
2020-06-13 00:36:43 +0530

    db connection

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -22,8 +22,8 @@ function openMongo(callback,host) {
 
     // var url=    'mongodb://localhost:27017/demoApp'
     console.log(process.env.DB)
-    var url = 'mongodb://' + process.env.DB_Address + ':' + process.env.DB_PORT + '/' + process.env.DB;
-    
+    // var url = 'mongodb://' + process.env.DB_Address + ':' + process.env.DB_PORT + '/' + process.env.DB;
+    var url ='mongodb://ebasicstest:ebasicstest@ebasics-test-shard-00-00-dnrmf.mongodb.net:27017,ebasics-test-shard-00-01-dnrmf.mongodb.net:27017,ebasics-test-shard-00-02-dnrmf.mongodb.net:27017/articlepost?ssl=true&replicaSet=eBasics-test-shard-0&authSource=admin'
     console.log(url)
     mongoose.connect(url, {
         useCreateIndex: true, useNewUrlParser: true,useUnifiedTopology: true
