d773f7fe4d6ae867ae36d0d446c5cfe051ffb04a
pradeepb014@gmail.com <pradeepb014@gmail.com>
2020-06-13 00:43:12 +0530

    8085

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
      "IP_ADDRESS": "124.7.41.184", // "210.18.127.24", // 223.30.223.177 --->check env file
-     "PORT": "8080", // --->check env file
+     "PORT": "8085", // --->check env file
     "secretKey" : 'yRQYnWzskCZUxPwaQupWkiUzKELZ49eM7oWxAQK_ZXw'
     };
 
